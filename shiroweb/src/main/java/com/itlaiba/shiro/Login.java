06919e4be959b992f82d6e2f7955ebd02c85078e
admin <937220832@qq.com>
2018-06-18 23:54:23 +0800

    shiro整合了web（servlet），主要在项目启动时加载了shiro的配置文件

---
+++ shiroweb/src/main/java/com/itlaiba/shiro/Login.java
@@ -0,0 +1,37 @@
+package com.itlaiba.shiro;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.subject.Subject;
+
+public class Login extends HttpServlet{
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		System.out.println("login doget");
+		resp.sendRedirect("login.jsp");
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		System.out.println("login dopost");
+		String username = req.getParameter("username");
+		String password = req.getParameter("password");
+		Subject subject = SecurityUtils.getSubject();
+		UsernamePasswordToken token = new UsernamePasswordToken(username, password);
+		try {
+			subject.login(token);
+			resp.sendRedirect("success.jsp");
+		} catch (Exception e) {
+			req.setAttribute("msg", "失败");
+			req.getRequestDispatcher("login.jsp").forward(req, resp);
+		}
+	}
+}
